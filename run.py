af39ed3c38d1d54ba584d1bda9c329f7bac8d2e2
= <=>
2021-02-02 03:45:03 +0300

    test

--- run.py
+++ run.py
@@ -45,7 +45,7 @@ def generate_password(update, context):
     symbols='1234567890-=qwertyuiopasdfghjklzxcvbnm,./!^&*()_+QWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>?`~[]'
     l = randrange(8,16)
     password_ = ""
-    for i in range(l):
+    for t in range(l):
         password_+= choice(symbols)
     context.bot.send_message(chat_id=update.effective_chat.id, text="Это твой пароль: "+password_+"\nНикому не сообщай его!")
 
