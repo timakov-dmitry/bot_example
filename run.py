5c3ff5c68e464776d6da4366e077305b3c6afe6b
ilnur786 <ilnur-qwerty@mail.ru>
2021-02-03 22:31:13 +0300

    реализовал функцию "что поесть"

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
 from modules.password_generator.module import generate_password
 from modules.image_classificator.module import on_image, set_threshold_handler
+from modules.what_eat.module import chose_food
 
 
 updater = Updater(token='1685734491:AAEMp2ZOAy6Hya0U9LKF28IhLQzhomNcXDA', use_context=True)
@@ -21,7 +22,8 @@ start_handler = CommandHandler('start', start)
 generate_password_handler = MessageHandler(Filters.text(['Пароль','пароль','password']), generate_password)
 set_threshold_handler = CommandHandler('set_threshold', set_threshold_handler)
 message_handler = MessageHandler(Filters.text & (~Filters.command), on_message)
-
+what_eat_handler = MessageHandler(Filters.text(['Что поесть',"Что выбрать поесть","еда","есть","поесть","что выбрать"]), chose_food)
+dispatcher.add_handler(what_eat_handler)
 dispatcher.add_handler(generate_password_handler)
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(message_handler)
