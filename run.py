1685e50e7abc497198ea7868a1b6e4451b1ec530
Daniches <danichees@ya.ru>
2021-02-07 23:11:58 +0300

    Added random Norbit vacancy handler, added information to starting messages

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from modules.password_generator.module import generate_password
 from modules.image_classificator.module import on_image, set_threshold_handler
 from modules.advice_generator.module import generate_advice
 from modules.what_eat.module import chose_food
+from modules.vacancies.module import send_vacancy
 
 
 updater = Updater(token='1685734491:AAEMp2ZOAy6Hya0U9LKF28IhLQzhomNcXDA', use_context=True)
@@ -13,6 +14,7 @@ dispatcher = updater.dispatcher
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Я могу находить объекты на изображениях")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="А если напишешь мне 'Вакансия' - я постараюсь найти вакансию твоей мечты в Норбит!")
 
 
 def on_message(update, context):
@@ -22,6 +24,7 @@ def on_message(update, context):
 start_handler = CommandHandler('start', start)
 generate_password_handler = MessageHandler(Filters.text(['Пароль','пароль','password']), generate_password)
 advice_generator_handler = MessageHandler(Filters.text(['Дай совет','Что посоветуешь','совет']), generate_advice)
+vacancies_handler = MessageHandler(Filters.text(['Вакансия','Вакансии','Вакансии Курск']), send_vacancy)
 
 set_threshold_handler = CommandHandler('set_threshold', set_threshold_handler)
 message_handler = MessageHandler(Filters.text & (~Filters.command), on_message)
@@ -30,6 +33,7 @@ dispatcher.add_handler(what_eat_handler)
 dispatcher.add_handler(generate_password_handler)
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(advice_generator_handler)
+dispatcher.add_handler(vacancies_handler)
 dispatcher.add_handler(message_handler)
 dispatcher.add_handler(MessageHandler(Filters.photo, on_image))
 
