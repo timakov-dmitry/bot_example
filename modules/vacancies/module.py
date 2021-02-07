082015cfd86f5e845028663e79dc082772c0b75e
Daniches <danichees@ya.ru>
2021-02-07 23:12:19 +0300

    Added random vacancy generator

---
+++ modules/vacancies/module.py
@@ -0,0 +1,49 @@
+import requests
+from random import choice
+from bs4 import BeautifulSoup
+
+
+def random_vacancy():
+    
+    link = "https://www.norbit.ru/karera/vakansii/kursk/"
+    site = "https://www.norbit.ru"
+    departaments = ['departament-crm-kursk/',
+                'departament-hr-krsk/',
+                'vakansii-departamenta-microsoft-dynamics-ax-kursk/',
+                'departament-nbt-srm-krsk/',
+                'departament-lanit-omni-krsk/',
+                'departament-mashinnogo-obucheniya/',
+                'departament-obespecheniya-krsk/',
+                'departament-upravlencheskogo-konsaltinga/',
+                'otdel-administrativnogo-upravleniya-kursk/']
+  
+    vacancies = []
+
+    #Проходимся по всем департаментам на сайте
+    for departament in departaments:
+        soup = BeautifulSoup(requests.get(link + departament).text, 'html.parser')
+        
+        #Находим все публикации о вакансиях и собираем информацию о них в список
+        publications = soup.find_all('a', class_="title customfont")
+        for publication in publications:
+            image_href = site + publication.find('img')['src']
+            text = publication.text.strip()
+            href = site + publication['href']
+            vacancies.append({'img':image_href, 'text':text, 'link':href})
+    return (choice(vacancies))
+
+
+def send_vacancy(update, context):
+    
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Секундочку, ищу вакансию твоей мечты!☺️")
+    try:
+        vacancy = random_vacancy()
+        message_text = '\n{0}\n<a href="{1}"><i>Cсылка на вакансию</i></a>'.format(vacancy['text'],vacancy['link'])
+        
+        context.bot.sendPhoto(chat_id=update.effective_chat.id, 
+                              photo = vacancy['img'], 
+                              caption=message_text, 
+                              parse_mode='HTML')
+    except:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Прости, что-то пошло не так😥")
+    
