b2876e09cc362d92d0d5c54852ae621f7398e738
ch3rny1 <75674002+ch3rny1@users.noreply.github.com>
2021-02-08 23:18:01 +0300

    Update run.py

--- run.py
+++ run.py
@@ -7,6 +7,7 @@ from modules.news.module import generate_news_module
 from modules.password_generator.module import generate_password_module
 from modules.what_eat.module import chose_food_module
 from modules.vacancies.module import random_vacancy_module
+from modules.film_generator.module import generate_film_module
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -20,6 +21,7 @@ bot.register(generate_advice_module, ['Дай совет','Что посовет
 bot.register(chose_food_module, ['Что поесть',"Что выбрать поесть","еда","есть","поесть","что выбрать"], "А если напишешь мне 'еда' - я дам совет по еде!")
 bot.register(random_vacancy_module, ['Вакансия','Вакансии','Вакансии Курск', 'вакансия'], "А если напишешь мне 'вакансия' - я постараюсь найти вакансию твоей мечты в Норбит!")
 bot.register(generate_news_module, ['Новость','новость','новости', 'Новости'], "А если напишешь мне 'новость' - я расскажу тебе последнюю новость в России!")
+bot.register(generate_film_module, ['Фильм','фильм','Кино', 'Фильмы'], "А если напишешь мне 'фильм' - я предложу тебе фильм!")
 
 bot.run()
 
