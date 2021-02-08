d4802da92329972f04f0b7a76cdfe0dd7d59de12
Max Gepolov <gepolov@gmail.com>
2021-02-08 19:44:29 +0300

    Добавлен модуль новостей

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from bot import Bot
 
 from modules.image_classificator.module import object_detection_module
 from modules.advice_generator.module import generate_advice_module
+from modules.news.module import generate_news_module
 from modules.password_generator.module import generate_password_module
 from modules.what_eat.module import chose_food_module
 from modules.vacancies.module import random_vacancy_module
@@ -18,6 +19,7 @@ bot.register(object_detection_module, ['Пароль', 'пароль', 'password
 bot.register(generate_advice_module, ['Дай совет','Что посоветуешь','совет'], "А если напишешь мне 'совет' - я дам совет!")
 bot.register(chose_food_module, ['Что поесть',"Что выбрать поесть","еда","есть","поесть","что выбрать"], "А если напишешь мне 'еда' - я дам совет по еде!")
 bot.register(random_vacancy_module, ['Вакансия','Вакансии','Вакансии Курск', 'вакансия'], "А если напишешь мне 'вакансия' - я постараюсь найти вакансию твоей мечты в Норбит!")
+bot.register(generate_news_module, ['Новость','новость','новости', 'Новости'], "А если напишешь мне 'новость' - я расскажу тебе последнюю новочть в России!")
 
 bot.run()
 
